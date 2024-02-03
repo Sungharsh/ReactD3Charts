17d1e737fb757be07d658430e6997cb5faf4cd62
Sungharsh <suncam123@gmail.com>
2024-02-03 13:38:03 +0000

    Added vertical bars based on years and prices from JSON file

--- src/barBhart.tsx
+++ src/barBhart.tsx
@@ -26,13 +26,37 @@ const BarChart = () => {
       .range([0, width])
       .padding(0.3);
 
+    const xScaleWithIndex = d3
+      .scaleBand<number>()
+      .domain(priceHistory1.map((_d, i) => i))
+      .range([0, width])
+      .padding(0.3);
+
     const yScale = d3
       .scaleLinear<number>()
       .domain([0, d3.max(priceHistory1.map(values => values.price)) as number])
       .range([height, 30]);
 
-    svg.append("g").attr("transform", `translate(0, ${height})`).call(d3.axisBottom(xScaleWithYears));
-    svg.append("g").attr("transform", `translate(${margin.left}, ${margin.top})`).call(d3.axisLeft(yScale));
+    svg.append("g").attr("transform", `translate(8, ${height})`).call(d3.axisBottom(xScaleWithYears));
+    svg
+      .append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top - 20})`)
+      .call(d3.axisLeft(yScale));
+
+    const bar = svg
+      .selectAll(".bar")
+      .data(priceHistory1.map(values => values.price))
+      .enter();
+
+    bar
+      .append("g")
+      .attr("transform", "translate(10, 0)")
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", (_d, i) => xScaleWithIndex(i) as number)
+      .attr("y", d => yScale(d))
+      .attr("width", xScaleWithYears.bandwidth())
+      .attr("height", d => (height - yScale(d)) as number);
 
     return () => {
       d3.select("svg").remove();
