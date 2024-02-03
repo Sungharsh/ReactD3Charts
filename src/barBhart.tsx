224c9f05d8e0f9103ab46a740da5d6ecd53e467c
Sungharsh <suncam123@gmail.com>
2024-02-03 13:17:02 +0000

    Created X and Y axis on chart

--- src/barBhart.tsx
+++ src/barBhart.tsx
@@ -1,5 +1,6 @@
 import { useRef, useEffect } from "react";
 import * as d3 from "d3";
+import priceHistory1 from "./price-history1.json";
 
 const BarChart = () => {
   const chartRef = useRef(null);
@@ -11,16 +12,27 @@ const BarChart = () => {
     const height = 400;
     const margin = { top: 20, right: 20, bottom: 20, left: 20 };
 
-    d3.select(chartElement)
+    const svg = d3
+      .select(chartElement)
       .append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
-      .append("text")
-      .attr("x", 10)
-      .attr("y", 50)
-      .text("This is D3 SVG sss")
-      .attr("fill", "white")
-      .attr("class", "title");
+      .append("g")
+      .attr("transform", "translate(15, 0)");
+
+    const xScaleWithYears = d3
+      .scaleBand<number>()
+      .domain(priceHistory1.map(values => values.year))
+      .range([0, width])
+      .padding(0.3);
+
+    const yScale = d3
+      .scaleLinear<number>()
+      .domain([0, d3.max(priceHistory1.map(values => values.price)) as number])
+      .range([height, 30]);
+
+    svg.append("g").attr("transform", `translate(0, ${height})`).call(d3.axisBottom(xScaleWithYears));
+    svg.append("g").attr("transform", `translate(${margin.left}, ${margin.top})`).call(d3.axisLeft(yScale));
 
     return () => {
       d3.select("svg").remove();
