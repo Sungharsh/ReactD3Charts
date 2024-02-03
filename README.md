4d58d14b60b93160342ea0044d07af5e4fa09bdf
Sungharsh <suncam123@gmail.com>
2024-02-03 09:11:28 +0000

    added width and height to svg

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ReactD3Charts
+
+# I am currently develping this app, I will update README later
+
 A Reactjs and D3js application to to present and dynamically visualize data in the form of a bar chart.
--- src/barBhart.tsx
+++ src/barBhart.tsx
@@ -5,15 +5,21 @@ const BarChart = () => {
   const chartRef = useRef(null);
 
   useEffect(() => {
-    //if conditional is a temporary setup for devlopment only to resolve svg rending twice due to React.StrictMode
+    //if conditional is a temporary setup for devlopment only to resolve svg rendering twice due to React.StrictMode
     if (d3.select("svg")) {
       d3.select("svg").remove();
     }
 
     const chartElement = chartRef.current;
 
+    const width = 500;
+    const height = 400;
+    const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+
     d3.select(chartElement)
       .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
       .append("text")
       .attr("x", 10)
       .attr("y", 50)
