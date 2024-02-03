efda0e35f4f6e9c6ee8f9b5cb4e7db109710335a
Sungharsh <suncam123@gmail.com>
2024-02-03 14:55:03 +0000

    Added titles on x, y axis and title on top of the chart

--- src/barChart.tsx
+++ src/barChart.tsx
@@ -8,9 +8,9 @@ const BarChart = () => {
   useEffect(() => {
     const chartElement = chartRef.current;
 
-    const width = 500;
-    const height = 400;
-    const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+    const margin = { top: 22, right: 30, bottom: 50, left: 35 };
+    const width = 550 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
 
     const svg = d3
       .select(chartElement)
@@ -18,7 +18,7 @@ const BarChart = () => {
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
-      .attr("transform", "translate(15, 0)");
+      .attr("transform", `translate(${margin.left + 15}, ${margin.top})`);
 
     const xScaleWithYears = d3
       .scaleBand<number>()
@@ -37,11 +37,39 @@ const BarChart = () => {
       .domain([0, d3.max(priceHistory1.map(values => values.price)) as number])
       .range([height, 30]);
 
-    svg.append("g").attr("transform", `translate(8, ${height})`).call(d3.axisBottom(xScaleWithYears));
+    svg
+      .attr("transform", `translate(${margin.left + 18}, ${margin.top})`)
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`)
+      .append("text")
+      .attr("transform", "translate(100, 0)")
+      .attr("x", -10)
+      .attr("y", -15)
+      .attr("class", "title")
+      .text("Price Chart");
     svg
       .append("g")
-      .attr("transform", `translate(${margin.left}, ${margin.top - 20})`)
-      .call(d3.axisLeft(yScale));
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(xScaleWithYears))
+      .append("text")
+      .attr("y", height - 295)
+      .attr("x", width - 260)
+      .text("Years")
+      .attr("class", "xHeadding");
+
+    svg
+      .append("g")
+      .attr("transform", `translate(${margin.left - 35}, ${margin.top - 20})`)
+      .call(d3.axisLeft(yScale))
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("x", width - 580)
+      .attr("y", height - 40)
+      .attr("dy", "-23.1em")
+      .attr("class", "yHeadding")
+      .text("Stock price");
 
     const bar = svg
       .selectAll(".bar")
